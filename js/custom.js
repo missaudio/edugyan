95efce961ea3a3678a7d3045ea667c7a37e68391
Swati Mandal <99341263+missaudio@users.noreply.github.com>
2022-02-20 18:28:07 +0530

    done

--- js/custom.js
+++ js/custom.js
@@ -268,3 +268,17 @@ function initMap() {
     icon: image
   });
 }
+
+
+
+// PopUp
+
+function popupOpen() {
+  document.getElementById("popup").style.display = "block";
+  document.getElementById("overlay").style.display = "block";
+}
+// Popup Close
+function popupClose() {
+  document.getElementById("popup").style.display = "none";
+  document.getElementById("overlay").style.display = "none";
+}
